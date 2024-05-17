b8dd9bd8cb99940abc36ed9205e0c34e12eb5a9f
unknown <negi60239@gmail.com>
2024-05-17 18:11:43 +0530

    task2

--- src/components/SortTable.js
+++ src/components/SortTable.js
@@ -4,10 +4,16 @@ import React, { useState } from 'react'
 const DateAndViewsTable = () => {
 
     const initialData = [
-        { date: '2024-05-14', views: 120 },
-        { date: '2024-04-20', views: 95 },
-        { date: '2024-05-10', views: 130 },
-        { date: '2024-03-22', views: 150 },
+       
+    { date: "2022-09-01", views: 100, article: "Article 1" },
+
+    { date: "2023-09-01", views: 100, article: "Article 1" },
+
+    { date: "2023-09-02", views: 150, article: "Article 2" },
+
+    { date: "2023-09-02", views: 120, article: "Article 3" },
+
+    { date: "2020-09-03", views: 200, article: "Article 4" }
       ];
 
 
@@ -39,6 +45,7 @@ const DateAndViewsTable = () => {
                 <tr>
                   <th>Date</th>
                   <th>Views</th>
+                  <th>Article</th>
                 </tr>
               </thead>
 
@@ -49,6 +56,7 @@ const DateAndViewsTable = () => {
                      
                     <td>{item.date}</td>
                     <td>{item.views}</td>
+                    <td>{item.article}</td>
 
                   </tr>
                    
